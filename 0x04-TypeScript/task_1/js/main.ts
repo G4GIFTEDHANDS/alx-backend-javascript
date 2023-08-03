7f97b0883d4eabe6dad2aed0adf3537405c9ddcf
G4GIFTEDHANDS <nwachukwuonyinyechi79@gmail.com>
2023-08-03 01:54:10 +0000

    0x04-TypeScript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,53 +1,45 @@
-// task_5 Advanced types Part 1
-interface DirectorInterface {
-  workFromHome(): string
+//Task_1 Let's build a Teacher interface
+interface Teacher {
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience: number,
+  location: string,
 
-  getCoffeeBreak(): string
+  [propName: string]: any
+  }
+
+// Task_2 Extending the Teacher class
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Task_3 Printing teachers
+interface printTeacherFunction {
+  (firstName: string, lastName: string ): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string ): string => `${firstName.charAt(0)}. ${lastName}`;
+// Task_4 Writing a class
+interface classInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
   
-  workDirectorTasks(): string
-
-}
-
-interface TeacherInterface {
-  workFromHome(): string
-
-  getCoffeeBreak(): string
-
-  workTeacherTasks(): string
-}
-
-export class Director implements DirectorInterface {
-  workFromHome = (): string => "Working from home";
-
-  getCoffeeBreak = (): string => "Getting a coffee break";
-
-  workDirectorTasks = (): string => "Getting to director tasks";
-}
-
-export class Teacher implements TeacherInterface {
-  workFromHome = (): string => "Cannot work from home";
-
-  getCoffeeBreak = (): string => "Cannot have a break";
-
-  workTeacherTasks = (): string => "Getting to work";
-}
-
-export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
-
-// task_6 Creating functions specific to employees
-export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
-  return (employee as Director).workDirectorTasks() !== undefined;
-}
-
-export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-  let res = undefined;
-  (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
-  return res;
-}
-
-// task_7  String literal types
-type Subjects = "Math" | "History";
-
-export function teachClass(todayClass: Subjects): string | boolean {
-  return (todayClass === "Math") ? "Teaching Math" : (todayClass === "History") ? "Teaching History" : `${undefined}`
 }
